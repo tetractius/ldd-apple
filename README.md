a245ed7546c6bda26460e7e1e465100520f5860f
Filippo Trimoldi <filippotrimoldi@nianticlabs.com>
2022-10-28 15:39:26 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ldd-apple
 A more ldd like tool for macos to investigate dylib dependencies
+
+I am more of a linux guy and I was annoyed by the fact that apply does not have a real alternative to `ldd`. There is `otool -L` of course but it performs a static analysis that does not really tell you want might happen at runtime like ld do (see https://stackoverflow.com/a/67818942/6189172)
+
+So starting from the idea of Cris (https://stackoverflow.com/a/56248596/6189172) and overcoming the problem [SIP](https://apple.co/3sDkQZJ) that would not allow some environemt variable to work properly for security reason, I came up with this:
+
+```shell
+ldd-apple.sh [either a dylib or an executable]
+```
+
+This basically injects a dynamic library while trying to run simple executable that print environment variables, with the idea of trying to see if the library can be loaded at runtime.
+
+Ah... and there in one more thing... it also work with an executable, if it is not restricted. You can check if it restricted by running on macOS
+
+```shell
+/bin/ls -lOL <executable>
+```
+
+Most of the command like tools for example are restricted...
+
+
+```shell
+❯ /bin/ls -lOL /usr/bin/true
+-rwxr-xr-x  1 root  wheel  restricted,compressed 100512 24 Aug 09:59 /usr/bin/true
+```
---
+++ ldd-apple.sh
@@ -0,0 +1,68 @@
+#!/bin/bash 
+
+# Checking if file is an executable
+file $1 | grep executable
+IS_EXECUTABLE=$?
+
+if [ "$IS_EXECUTABLE" == "0" ]
+then
+
+  /bin/ls -lOL $1 | grep restricted
+  IS_EXECUTABLE_RESTRICTED=$?
+  if [ "$IS_EXECUTABLE" == "0" ]
+  then
+    echo "===== The executable $1 is restricted, so no information at runtime, sorry. More info at https://apple.co/3sDkQZJ"
+    exit
+  fi
+  echo "========== Trying to execute the binary while printing library linked:"
+  DYLD_PRINT_LIBRARIES=1 \
+  DYLD_PRINT_LIBRARIES_POST_LAUNCH=1 \
+  DYLD_PRINT_RPATHS=$1 \
+  $1
+  exit 0
+fi
+
+temp_unrestricted_source=$(mktemp /tmp/unrestricted.$(uuidgen).cpp)
+temp_unrestricted_out="${temp_unrestricted_source}.exec"
+
+# Creating source file for an unrestricted binary
+echo "#include <stdio.h>
+int main(int argc, char **argv, char **envp) {
+    printf(\"%s\n\", \"========== Showing environment variables:\");
+    while (*envp) {
+        printf(\"%s\n\", *envp);
+        envp++;
+    }
+    return 0;
+}" > ${temp_unrestricted_source}
+
+ALL_ARCHS_IN_BINARY=`lipo -info $1 | awk 'BEGIN{FS=":"}{print $3}'`
+
+for arch in `echo $ALL_ARCHS_IN_BINARY`; do
+  echo "===== Found $arch architecture"
+  temp_unrestricted_out="${temp_unrestricted_out}.${arch}"
+
+  echo "========== The following will be temporatily created:"
+  echo $temp_unrestricted_source
+  echo $temp_unrestricted_out
+
+  # Very hard assumption that llvm keeps the target architecture name consistent
+  clang $temp_unrestricted_source -target ${arch}-apple-macos11 -o $temp_unrestricted_out
+
+  while getopts "r" OPTION; do
+    case $OPTION in   
+      r) export DYLD_PRINT_RPATHS=1;;
+    esac
+  done
+  shift $((OPTIND-1))
+
+  echo "========== Showing runtime library dependencies:"
+  DYLD_PRINT_LIBRARIES=1 \
+  DYLD_PRINT_LIBRARIES_POST_LAUNCH=1 \
+  DYLD_INSERT_LIBRARIES=$1 \
+  $temp_unrestricted_out
+
+  rm -f $temp_unrestricted_out
+done
+
+rm -f $temp_unrestricted_source
